b2a48b856cb605dc3743b376e1c8c79199563a5a
Yasmin Eduarda Piontkievicz <160152483+yasminpiontkievicz@users.noreply.github.com>
2024-06-14 12:36:59 -0300

    Add files via upload

---
+++ operacaoaritmetica/script.js
@@ -0,0 +1,20 @@
+var y = 25;
+var x = 50; 
+
+var soma = y + x; 
+var subtracao = y - x;
+var multiplicacao = y * x; 
+var divisao = y / x; 
+var modulo = y % x;
+
+   // console.log(soma);
+ /*  console.log(subtracao);*/
+  //console.log(multiplicacao);
+ /*console.log(divisao);*/
+  //console.log(modulo);
+
+a = y++; 
+b = x--;
+
+console.log(a);
+console.log(b);
